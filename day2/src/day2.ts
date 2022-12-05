9354e2282df0b64de30f005d4cb63b7ab4359774
Capopato <107575282+Capopato@users.noreply.github.com>
2022-12-05 18:51:54 +0100

    Add files via upload

--- day2/src/day2.ts
+++ day2/src/day2.ts
@@ -15,7 +15,7 @@ class Day2{
         })
     }
 
-    workData = () => {
+    part1 = () => {
         let draw: number = 3
         let win: number = 6
         let rock: number = 1
@@ -56,6 +56,52 @@ class Day2{
                 count += paper
             }
         }
+        console.log(count)
+        return count
+    }
+
+    part2 = () => {
+        let lose: number = 0
+        let draw: number = 3
+        let win: number = 6
+        let rock: number = 1
+        let paper: number = 2
+        let scissor: number = 3
+        let count: number = 0
+
+        for(let i = 0; i < this.splitArr.length; i++){
+            //Rock conditions
+            if (this.splitArr[i][0] == 'A' && this.splitArr[i][1] == 'X'){
+                count += lose + scissor
+            }
+            else if (this.splitArr[i][0] == 'A' && this.splitArr[i][1] == 'Y'){
+                count += draw + rock
+            }
+            else if (this.splitArr[i][0] == 'A' && this.splitArr[i][1] == 'Z'){
+                count += win + paper
+            }
+            //Paper conditions
+            else if (this.splitArr[i][0] == 'B' && this.splitArr[i][1] == 'X'){
+                count += lose + rock
+            }
+            else if (this.splitArr[i][0] == 'B' && this.splitArr[i][1] == 'Y'){
+                count += draw + paper
+            }
+            else if (this.splitArr[i][0] == 'B' && this.splitArr[i][1] == 'Z'){
+                count += win + scissor
+            }
+            //Scissor conditions
+            else if (this.splitArr[i][0] == 'C' && this.splitArr[i][1] == 'X'){
+                count += lose + paper
+            }
+            else if (this.splitArr[i][0] == 'C' && this.splitArr[i][1] == 'Y'){
+                count += draw + scissor
+            }
+            else if (this.splitArr[i][0] == 'C' && this.splitArr[i][1] == 'Z'){
+                count += win + rock
+            }
+        }
+        console.log(count)
         return count
     }
 }
@@ -63,4 +109,5 @@ class Day2{
 let day2 = new Day2
 day2.importData()
 day2.splitArray()
-day2.workData()
+// day2.part1()
+day2.part2()
