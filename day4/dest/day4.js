41432dc4b80f074757d07a2782dceecdb2559c24
Capopato <107575282+Capopato@users.noreply.github.com>
2022-12-24 15:46:41 +0100

    Add files via upload

---
+++ day4/dest/day4.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+let count = 0;
+const assignmentPairs = (0, fs_1.readFileSync)('./input.txt', { encoding: "utf-8" });
+const splitOfPairs = assignmentPairs.split('\n');
+const splitPerPair = [];
+// split per pair and convert to number
+splitOfPairs.map((twoPairs) => {
+    const pairs = twoPairs.split(',');
+    splitPerPair.push({
+        pairOne: pairs[0].split('-').map(Number),
+        pairTwo: pairs[1].split('-').map(Number)
+    });
+});
+for (let i = 0; i < splitPerPair.length; i++) {
+    // Create constant of start/end num of pair one and two
+    const pairOneStartNum = splitPerPair[i].pairOne[0];
+    const pairOneEndNum = splitPerPair[i].pairOne[1];
+    const pairTwoStartNum = splitPerPair[i].pairTwo[0];
+    const pairTwoEndNum = splitPerPair[i].pairTwo[1];
+    // Check if startnum pair one is lower that startnum pair two and visa versa
+    // And check if endnum pair one is higher than endnum pair two and visa versa
+    if (pairOneStartNum <= pairTwoStartNum && pairOneEndNum >= pairTwoEndNum) {
+        count += 1;
+    }
+    else if (pairTwoStartNum <= pairOneStartNum && pairTwoEndNum >= pairOneEndNum) {
+        count += 1;
+    }
+}
+console.log(count);
---
+++ day4/src/day4.ts
@@ -0,0 +1,38 @@
+import { readFileSync } from 'fs'
+
+let count: number = 0
+const assignmentPairs: string = readFileSync('./input.txt', {encoding: "utf-8"})
+const splitOfPairs: string[] = assignmentPairs.split('\n')
+
+const splitPerPair: splitPerPairOptions[] = []
+interface splitPerPairOptions {
+    pairOne: number[]
+    pairTwo: number[]
+}
+
+// split per pair and convert to number
+splitOfPairs.map((twoPairs: string) => {
+    const pairs: string[] = twoPairs.split(',')
+    splitPerPair.push({
+        pairOne: pairs[0].split('-').map(Number),
+        pairTwo: pairs[1].split('-').map(Number)
+    })
+})
+
+for (let i = 0; i < splitPerPair.length; i++) {
+    // Create constant of start/end num of pair one and two
+    const pairOneStartNum: number = splitPerPair[i].pairOne[0]
+    const pairOneEndNum: number = splitPerPair[i].pairOne[1]
+    const pairTwoStartNum: number = splitPerPair[i].pairTwo[0]
+    const pairTwoEndNum: number = splitPerPair[i].pairTwo[1]
+
+    // Check if startnum pair one is lower that startnum pair two and visa versa
+    // And check if endnum pair one is higher than endnum pair two and visa versa
+    if (pairOneStartNum <= pairTwoStartNum && pairOneEndNum >= pairTwoEndNum) {
+        count += 1 
+    }
+    else if (pairTwoStartNum <= pairOneStartNum && pairTwoEndNum >= pairOneEndNum) {
+        count += 1
+    }
+}
+console.log(count)
