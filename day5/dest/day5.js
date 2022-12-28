a499663556fab798c25f810ef374f1e7b6faddf1
Capopato <107575282+Capopato@users.noreply.github.com>
2022-12-28 08:11:07 +0100

    Add files via upload

--- day5/dest/day5.js
+++ day5/dest/day5.js
@@ -1,136 +1,196 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
-let crateArrangementInput = (0, fs_1.readFileSync)('./input.txt', { encoding: "utf-8" }).split('\n');
-const moves = [];
-const startArrangement = [];
-// Convert input and push the start arrangement to array
-// Push the moves to a different array
-crateArrangementInput.map(column => {
-    let index = 1;
-    if (column.split(' ')[0] == 'move') {
-        moves.push(column);
+const getDataInput = () => {
+    let crateArrangementInput = (0, fs_1.readFileSync)('./input.txt', { encoding: "utf-8" }).split('\n');
+    return crateArrangementInput;
+};
+const orderOfTheCrateArrangement = () => {
+    const crateArrangementInput = getDataInput();
+    const moves = [];
+    const startArrangement = [];
+    // Convert input and push the start arrangement to array
+    // Push the moves to a different array
+    crateArrangementInput.map(column => {
+        let index = 1;
+        if (column.split(' ')[0] == 'move') {
+            moves.push(column);
+        }
+        else {
+            // Index + 4 because in the string the space between every value is 4 spaces
+            for (let i = 1; i < column.length; i++) {
+                const row1 = column[index];
+                index += 4;
+                const row2 = column[index];
+                index += 4;
+                const row3 = column[index];
+                index += 4;
+                const row4 = column[index];
+                index += 4;
+                const row5 = column[index];
+                index += 4;
+                const row6 = column[index];
+                index += 4;
+                const row7 = column[index];
+                index += 4;
+                const row8 = column[index];
+                index += 4;
+                const row9 = column[index];
+                // Push the column to array 
+                startArrangement.push({
+                    row1: row1,
+                    row2: row2,
+                    row3: row3,
+                    row4: row4,
+                    row5: row5,
+                    row6: row6,
+                    row7: row7,
+                    row8: row8,
+                    row9: row9,
+                });
+                return;
+            }
+        }
+    });
+    let row1 = [];
+    let row2 = [];
+    let row3 = [];
+    let row4 = [];
+    let row5 = [];
+    let row6 = [];
+    let row7 = [];
+    let row8 = [];
+    let row9 = [];
+    // Push the start arrangement in top > bottom order to array
+    startArrangement.map(rows => {
+        if (rows.row1 != ' ') {
+            row1.push(rows.row1);
+        }
+        if (rows.row2 != ' ') {
+            row2.push(rows.row2);
+        }
+        if (rows.row3 != ' ') {
+            row3.push(rows.row3);
+        }
+        if (rows.row4 != ' ') {
+            row4.push(rows.row4);
+        }
+        if (rows.row5 != ' ') {
+            row5.push(rows.row5);
+        }
+        if (rows.row6 != ' ') {
+            row6.push(rows.row6);
+        }
+        if (rows.row7 != ' ') {
+            row7.push(rows.row7);
+        }
+        if (rows.row8 != ' ') {
+            row8.push(rows.row8);
+        }
+        if (rows.row9 != ' ') {
+            row9.push(rows.row9);
+        }
+    });
+    // Create object of all the rows
+    const totalRows = [row1, row2, row3, row4, row5, row6, row7, row8, row9,];
+    return totalRows;
+};
+const movesOfTheCrates = () => {
+    const crateArrangementInput = getDataInput();
+    const moves = [];
+    crateArrangementInput.map(column => {
+        if (column.split(' ')[0] == 'move') {
+            moves.push(column);
+        }
+    });
+    // All the numbers from the string will be pushed to this array
+    let allTheNumsFromMoves = [];
+    // Check the string for numbers and if word == a number push to allTheNumsFromMoves
+    moves.map(move => {
+        const movesSplitPerWord = move.split(' ');
+        for (let word of movesSplitPerWord) {
+            if (word != ' ' && word != 'move' && word != 'from' && word != 'to') {
+                allTheNumsFromMoves.push(word);
+            }
+        }
+    });
+    // Movement instructions will be pushed to this param
+    let numbersPerMovementRow = [];
+    // Split allTheNumsFromMoves per 3 index because the movement instructions goes per 3 numbers
+    // Push per 3 movement instructions to a new array so that it is easier to work with.
+    for (let i = 0; i < allTheNumsFromMoves.length; i += 3) {
+        let rows = allTheNumsFromMoves.slice(i, i + 3);
+        numbersPerMovementRow.push(rows);
     }
-    else {
-        // Index + 4 because in the string the space between every value is 4 spaces
-        for (let i = 1; i < column.length; i++) {
-            const row1 = column[index];
-            index += 4;
-            const row2 = column[index];
-            index += 4;
-            const row3 = column[index];
-            index += 4;
-            const row4 = column[index];
-            index += 4;
-            const row5 = column[index];
-            index += 4;
-            const row6 = column[index];
-            index += 4;
-            const row7 = column[index];
-            index += 4;
-            const row8 = column[index];
-            index += 4;
-            const row9 = column[index];
-            // Push the column to array 
-            startArrangement.push({
-                row1: row1,
-                row2: row2,
-                row3: row3,
-                row4: row4,
-                row5: row5,
-                row6: row6,
-                row7: row7,
-                row8: row8,
-                row9: row9,
-            });
-            return;
+    return numbersPerMovementRow;
+};
+/**
+ * Part One
+ */
+const funPartOne = () => {
+    const totalRows = orderOfTheCrateArrangement();
+    const numbersPerMovementRow = movesOfTheCrates();
+    // Use the numbers in the numbersPerMovementRowstring[] as input/code to get the movement coordinations.
+    for (let i = 0; i < (numbersPerMovementRow.length); i++) {
+        // The first index is the total amount of crates that needs to be moves 
+        const amountToMove = +numbersPerMovementRow[i][0];
+        // The second index is from which row the crates come
+        const fromRow = +numbersPerMovementRow[i][1];
+        // The third index is the row to which the crates go
+        const toRow = +numbersPerMovementRow[i][2];
+        // toBeSpliced is the values that will be spliced from the fromRow value (-1 because indexing start at 0)
+        const toBeSpliced = totalRows[fromRow - 1].splice(0, amountToMove);
+        // addToThisRow is the value of the toRow (-1 because indexing start at 0)
+        const addToThisRow = totalRows[toRow - 1];
+        // For loop the iterate over every individual value of toBeSpliced 
+        for (let x of toBeSpliced) {
+            // Reverse is because push put it at the end of the array but it needs to be at the front
+            addToThisRow.reverse().push(x);
+            // Reverse it back for the new splicing
+            addToThisRow.reverse();
         }
     }
-});
-let row1 = [];
-let row2 = [];
-let row3 = [];
-let row4 = [];
-let row5 = [];
-let row6 = [];
-let row7 = [];
-let row8 = [];
-let row9 = [];
-// Push the start arrangement in top > bottom order to array
-startArrangement.map(rows => {
-    if (rows.row1 != ' ') {
-        row1.push(rows.row1);
-    }
-    if (rows.row2 != ' ') {
-        row2.push(rows.row2);
-    }
-    if (rows.row3 != ' ') {
-        row3.push(rows.row3);
-    }
-    if (rows.row4 != ' ') {
-        row4.push(rows.row4);
-    }
-    if (rows.row5 != ' ') {
-        row5.push(rows.row5);
-    }
-    if (rows.row6 != ' ') {
-        row6.push(rows.row6);
-    }
-    if (rows.row7 != ' ') {
-        row7.push(rows.row7);
-    }
-    if (rows.row8 != ' ') {
-        row8.push(rows.row8);
-    }
-    if (rows.row9 != ' ') {
-        row9.push(rows.row9);
-    }
-});
-// Create object of all the rows
-const totalRows = [row1, row2, row3, row4, row5, row6, row7, row8, row9,];
-// All the numbers from the string will be pushed to this array
-let allTheNumsFromMoves = [];
-// Check the string for numbers and if word == a number push to allTheNumsFromMoves
-moves.map(move => {
-    const movesSplitPerWord = move.split(' ');
-    for (let word of movesSplitPerWord) {
-        if (word != ' ' && word != 'move' && word != 'from' && word != 'to') {
-            allTheNumsFromMoves.push(word);
+    return totalRows;
+};
+const funPartTwo = () => {
+    const totalRows = orderOfTheCrateArrangement();
+    const numbersPerMovementRow = movesOfTheCrates();
+    // Use the numbers in the numbersPerMovementRowstring[] as input/code to get the movement coordinations.
+    for (let i = 0; i < (numbersPerMovementRow.length); i++) {
+        // The first index is the total amount of crates that needs to be moves 
+        const amountToMove = +numbersPerMovementRow[i][0];
+        // The second index is from which row the crates come
+        const fromRow = +numbersPerMovementRow[i][1];
+        // The third index is the row to which the crates go
+        const toRow = +numbersPerMovementRow[i][2];
+        // toBeSpliced is the values that will be spliced from the fromRow value (-1 because indexing start at 0)
+        const toBeSpliced = totalRows[fromRow - 1].splice(0, amountToMove).reverse();
+        // addToThisRow is the value of the toRow (-1 because indexing start at 0)
+        const addToThisRow = totalRows[toRow - 1];
+        // For loop the iterate over every individual value of toBeSpliced 
+        for (let x of toBeSpliced) {
+            // Reverse is because push put it at the end of the array but it needs to be at the front
+            addToThisRow.reverse().push(x);
+            // Reverse it back for the new splicing
+            addToThisRow.reverse();
         }
     }
-});
-// Movement instructions will be pushed to this param
-let numbersPerMovementRow = [];
-// Split allTheNumsFromMoves per 3 index because the movement instructions goes per 3 numbers
-// Push per 3 movement instructions to a new array so that it is easier to work with.
-for (let i = 0; i < allTheNumsFromMoves.length; i += 3) {
-    let rows = allTheNumsFromMoves.slice(i, i + 3);
-    numbersPerMovementRow.push(rows);
-}
-// Use the numbers in the numbersPerMovementRowstring[] as input/code to get the movement coordinations.
-for (let i = 0; i < (numbersPerMovementRow.length); i++) {
-    // The first index is the total amount of crates that needs to be moves 
-    const amountToMove = +numbersPerMovementRow[i][0];
-    // The second index is from which row the crates come
-    const fromRow = +numbersPerMovementRow[i][1];
-    // The third index is the row to which the crates go
-    const toRow = +numbersPerMovementRow[i][2];
-    // toBeSpliced is the values that will be spliced from the fromRow value (-1 because indexing start at 0)
-    const toBeSpliced = totalRows[fromRow - 1].splice(0, amountToMove);
-    // addToThisRow is the value of the toRow (-1 because indexing start at 0)
-    const addToThisRow = totalRows[toRow - 1];
-    // For loop the iterate over every individual value of toBeSpliced 
-    for (let x of toBeSpliced) {
-        // Reverse is because push put it at the end of the array but it needs to be at the front
-        addToThisRow.reverse().push(x);
-        // Reverse it back for the new splicing
-        addToThisRow.reverse();
+    return totalRows;
+};
+const answer = () => {
+    const totalRowsPartOne = funPartOne();
+    const totalRowsPartTwo = funPartTwo();
+    // Create empty string that the top values will be added in
+    let topValuesPartOne = '';
+    let topValuesPartTwo = '';
+    // Iterate over totalrows to get the top value and add it to topValues
+    for (let topValue of totalRowsPartOne) {
+        topValuesPartOne += topValue[0];
     }
-}
-// Create empty string that the top values will be added in
-let topValues = '';
-// Iterate over totalrows to get the top value and add it to topValues
-for (let topValue of totalRows) {
-    topValues += topValue[0];
-}
-console.log(topValues);
+    for (let topValue of totalRowsPartTwo) {
+        topValuesPartTwo += topValue[0];
+    }
+    console.log(topValuesPartOne);
+    console.log(topValuesPartTwo);
+};
+answer();
--- day5/src/day5.ts
+++ day5/src/day5.ts
@@ -1,134 +1,158 @@
 import { readFileSync } from 'fs'
 
-let crateArrangementInput: string[] = readFileSync('./input.txt', {encoding: "utf-8"}).split('\n')
-const moves: string[] =[]
-const startArrangement: startArrangementOptions[] = []
-interface startArrangementOptions{
-    row1: string
-    row2: string
-    row3: string
-    row4: string
-    row5: string
-    row6: string
-    row7: string
-    row8: string
-    row9: string
+const getDataInput = () => {
+    let crateArrangementInput: string[] = readFileSync('./input.txt', {encoding: "utf-8"}).split('\n')
+    return crateArrangementInput
 }
 
-// Convert input and push the start arrangement to array
-// Push the moves to a different array
-crateArrangementInput.map(column => {
-    let index: number = 1
-
-    if (column.split(' ')[0] == 'move') {
-        moves.push(column)
+const orderOfTheCrateArrangement = () => {
+    const crateArrangementInput: string[]  = getDataInput()
+    const moves: string[] =[]
+    const startArrangement: startArrangementOptions[] = []
+    interface startArrangementOptions{
+        row1: string
+        row2: string
+        row3: string
+        row4: string
+        row5: string
+        row6: string
+        row7: string
+        row8: string
+        row9: string
     }
-    else{
-        // Index + 4 because in the string the space between every value is 4 spaces
-        for (let i = 1; i < column.length; i++) {
-            const row1: string = column[index]
-            index += 4
-            const row2: string = column[index]
-            index += 4
-            const row3: string = column[index]
-            index += 4
-            const row4: string = column[index]
-            index += 4
-            const row5: string = column[index]
-            index += 4
-            const row6: string = column[index]
-            index += 4
-            const row7: string = column[index]
-            index += 4
-            const row8: string = column[index]
-            index += 4
-            const row9: string = column[index]
-            // Push the column to array 
-            startArrangement.push({
-                row1: row1,
-                row2: row2,
-                row3: row3,
-                row4: row4,
-                row5: row5,
-                row6: row6,
-                row7: row7,
-                row8: row8,
-                row9: row9,
-            })
-            return
+    // Convert input and push the start arrangement to array
+    // Push the moves to a different array
+    crateArrangementInput.map(column => {
+        let index: number = 1
+
+        if (column.split(' ')[0] == 'move') {
+            moves.push(column)
         }
-    }
-})
-
-let row1: string[] = []
-let row2: string[] = []
-let row3: string[] = []
-let row4: string[] = []
-let row5: string[] = []
-let row6: string[] = []
-let row7: string[] = []
-let row8: string[] = []
-let row9: string[] = []
-
-// Push the start arrangement in top > bottom order to array
-startArrangement.map(rows => {
-    if (rows.row1 != ' '){
-        row1.push(rows.row1)
-    }
-    if (rows.row2 != ' '){
-        row2.push(rows.row2)
-    }
-    if (rows.row3 != ' '){
-        row3.push(rows.row3)
-    }
-    if (rows.row4 != ' '){
-        row4.push(rows.row4)
-    }
-    if (rows.row5 != ' '){
-        row5.push(rows.row5)
-    }
-    if (rows.row6 != ' '){
-        row6.push(rows.row6)
-    }
-    if (rows.row7 != ' '){
-        row7.push(rows.row7)
-    }
-    if (rows.row8 != ' '){
-        row8.push(rows.row8)
-    }
-    if (rows.row9 != ' '){
-        row9.push(rows.row9)
-    }
-})
-
-// Create object of all the rows
-const totalRows: string[][] = [row1, row2, row3, row4, row5, row6, row7, row8, row9,]
-
-// All the numbers from the string will be pushed to this array
-let allTheNumsFromMoves: string[] = []
-
-// Check the string for numbers and if word == a number push to allTheNumsFromMoves
-moves.map(move => {
-    const movesSplitPerWord = move.split(' ')
-    for (let word of movesSplitPerWord) {
-        if (word != ' ' && word != 'move' && word != 'from' && word != 'to'){
-            allTheNumsFromMoves.push(word)
+        else{
+            // Index + 4 because in the string the space between every value is 4 spaces
+            for (let i = 1; i < column.length; i++) {
+                const row1: string = column[index]
+                index += 4
+                const row2: string = column[index]
+                index += 4
+                const row3: string = column[index]
+                index += 4
+                const row4: string = column[index]
+                index += 4
+                const row5: string = column[index]
+                index += 4
+                const row6: string = column[index]
+                index += 4
+                const row7: string = column[index]
+                index += 4
+                const row8: string = column[index]
+                index += 4
+                const row9: string = column[index]
+                // Push the column to array 
+                startArrangement.push({
+                    row1: row1,
+                    row2: row2,
+                    row3: row3,
+                    row4: row4,
+                    row5: row5,
+                    row6: row6,
+                    row7: row7,
+                    row8: row8,
+                    row9: row9,
+                })
+                return
+            }
         }
-    }
-})
+    })
 
-// Movement instructions will be pushed to this param
-let numbersPerMovementRow: string[][] = []
+    let row1: string[] = []
+    let row2: string[] = []
+    let row3: string[] = []
+    let row4: string[] = []
+    let row5: string[] = []
+    let row6: string[] = []
+    let row7: string[] = []
+    let row8: string[] = []
+    let row9: string[] = []
 
-// Split allTheNumsFromMoves per 3 index because the movement instructions goes per 3 numbers
-// Push per 3 movement instructions to a new array so that it is easier to work with.
-for (let i = 0; i < allTheNumsFromMoves.length; i+=3 ){
-    let rows: string[] = allTheNumsFromMoves.slice(i,i+3)
-    numbersPerMovementRow.push(rows)
+    // Push the start arrangement in top > bottom order to array
+    startArrangement.map(rows => {
+        if (rows.row1 != ' '){
+            row1.push(rows.row1)
+        }
+        if (rows.row2 != ' '){
+            row2.push(rows.row2)
+        }
+        if (rows.row3 != ' '){
+            row3.push(rows.row3)
+        }
+        if (rows.row4 != ' '){
+            row4.push(rows.row4)
+        }
+        if (rows.row5 != ' '){
+            row5.push(rows.row5)
+        }
+        if (rows.row6 != ' '){
+            row6.push(rows.row6)
+        }
+        if (rows.row7 != ' '){
+            row7.push(rows.row7)
+        }
+        if (rows.row8 != ' '){
+            row8.push(rows.row8)
+        }
+        if (rows.row9 != ' '){
+            row9.push(rows.row9)
+        }
+    })
+
+    // Create object of all the rows
+    const totalRows: string[][] = [row1, row2, row3, row4, row5, row6, row7, row8, row9,]
+    return totalRows
 }
 
-// Use the numbers in the numbersPerMovementRowstring[] as input/code to get the movement coordinations.
-for (let i = 0; i < (numbersPerMovementRow.length); i++) {
+const movesOfTheCrates = () => {
+    const crateArrangementInput: string[]  = getDataInput()
+    const moves: string[] =[]
+    crateArrangementInput.map(column => {
+        if (column.split(' ')[0] == 'move') {
+            moves.push(column)
+        }
+    })
+    // All the numbers from the string will be pushed to this array
+    let allTheNumsFromMoves: string[] = []
+
+    // Check the string for numbers and if word == a number push to allTheNumsFromMoves
+    moves.map(move => {
+        const movesSplitPerWord = move.split(' ')
+        for (let word of movesSplitPerWord) {
+            if (word != ' ' && word != 'move' && word != 'from' && word != 'to'){
+                allTheNumsFromMoves.push(word)
+            }
+        }
+    })
+
+    // Movement instructions will be pushed to this param
+    let numbersPerMovementRow: string[][] = []
+
+    // Split allTheNumsFromMoves per 3 index because the movement instructions goes per 3 numbers
+    // Push per 3 movement instructions to a new array so that it is easier to work with.
+    for (let i = 0; i < allTheNumsFromMoves.length; i+=3 ){
+        let rows: string[] = allTheNumsFromMoves.slice(i,i+3)
+        numbersPerMovementRow.push(rows)
+    }
+    return numbersPerMovementRow
+}
+
+/**
+ * Part One
+ */
+const funPartOne = () => {
+    const totalRows = orderOfTheCrateArrangement()
+    const numbersPerMovementRow = movesOfTheCrates()
+
+    // Use the numbers in the numbersPerMovementRowstring[] as input/code to get the movement coordinations.
+    for (let i = 0; i < (numbersPerMovementRow.length); i++) {
     // The first index is the total amount of crates that needs to be moves 
     const amountToMove: number = +numbersPerMovementRow[i][0]
     // The second index is from which row the crates come
@@ -147,15 +171,56 @@ for (let i = 0; i < (numbersPerMovementRow.length); i++) {
         addToThisRow.reverse().push(x)
         // Reverse it back for the new splicing
         addToThisRow.reverse()
+        }
     }
+    return totalRows
+}   
+
+const funPartTwo = () => {
+    const totalRows = orderOfTheCrateArrangement()
+    const numbersPerMovementRow = movesOfTheCrates()
+
+    // Use the numbers in the numbersPerMovementRowstring[] as input/code to get the movement coordinations.
+    for (let i = 0; i < (numbersPerMovementRow.length); i++) {
+    // The first index is the total amount of crates that needs to be moves 
+    const amountToMove: number = +numbersPerMovementRow[i][0]
+    // The second index is from which row the crates come
+    const fromRow: number = +numbersPerMovementRow[i][1]
+    // The third index is the row to which the crates go
+    const toRow: number = +numbersPerMovementRow[i][2]
+    // toBeSpliced is the values that will be spliced from the fromRow value (-1 because indexing start at 0)
+    const toBeSpliced = totalRows[fromRow-1].splice(0, amountToMove).reverse()
+    // addToThisRow is the value of the toRow (-1 because indexing start at 0)
+    const addToThisRow = totalRows[toRow-1]
+    
+    // For loop the iterate over every individual value of toBeSpliced 
+    for (let x of toBeSpliced) {
+        // Reverse is because push put it at the end of the array but it needs to be at the front
+        addToThisRow.reverse().push(x)
+        // Reverse it back for the new splicing
+        addToThisRow.reverse()
+    }
+    }
+    return totalRows
 }
 
-// Create empty string that the top values will be added in
-let topValues: string = ''
+const answer = () => {
+    const totalRowsPartOne = funPartOne()
+    const totalRowsPartTwo = funPartTwo()
+    // Create empty string that the top values will be added in
+    let topValuesPartOne: string = ''
+    let topValuesPartTwo: string = ''
 
-// Iterate over totalrows to get the top value and add it to topValues
-for (let topValue of totalRows){
-    topValues += topValue[0]
+    // Iterate over totalrows to get the top value and add it to topValues
+    for (let topValue of totalRowsPartOne){
+        topValuesPartOne += topValue[0]
+    }
+    for (let topValue of totalRowsPartTwo){
+        topValuesPartTwo += topValue[0]
+    }
+
+    console.log(topValuesPartOne)
+    console.log(topValuesPartTwo)
 }
 
-console.log(topValues)
+answer()
